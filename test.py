5ad2151309dd03d08f9daae4f8e94a2cca2ddb29
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-06 16:49:23 +0000

    create test.py file to parse

---
+++ test.py
@@ -0,0 +1,16 @@
+# Armstrong Number Checker
+
+num = int(input("Enter a number: "))
+n = num
+power = len(str(num))
+total = 0
+
+while n > 0:
+    digit = n % 10
+    total += digit ** power
+    n //= 10
+
+if total == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
