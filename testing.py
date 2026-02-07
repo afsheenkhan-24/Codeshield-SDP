10daefac8a12ccca10285f995d98f37581e72783
Kevin Philip Oommen <kevinphilip320@gmail.com>
2026-02-07 08:22:57 +0000

    Unit testing

---
+++ testing.py
@@ -0,0 +1,25 @@
+import unittest
+from parser import calculate_complexity
+
+class TestComplexity(unittest.TestCase):
+
+    def test_script_1(self):
+        code = "print('Hello')"
+        self.assertEqual(calculate_complexity(code), 1)
+
+    def test_armstrong_logic(self):
+        # Your test.py has 1 'while' and 1 'if': M = 2 + 1 = 3
+        code = """
+                while n > 0:
+                    n //= 10
+                if total == num:
+                    print('Match')
+"""
+        self.assertEqual(calculate_complexity(code), 3)
+
+    def test_high_complexity(self):
+        code = "if 1>2: \n if 2>3: \n if 3>4: \n while True:"
+        self.assertEqual(calculate_complexity(code), 5)
+
+if __name__ == "__main__":
+    unittest.main()
