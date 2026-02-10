c0b142a6a2916b80df4f50603c01722e6d00ab97
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2026-02-10 09:50:25 +0000

    added tabs and navigation

---
+++ app.py
@@ -0,0 +1,28 @@
+from dashboard import Dashboard
+import streamlit as st
+from analysis import calculate_complexity
+
+def Complexity():
+    st.title("CodeShield: A Code Analysis and Security Tool")
+
+    tab1, tab2 = st.tabs(["Upload", "Paste Code"]) 
+
+    with tab1:
+        uploaded_file = st.file_uploader("Choose a .py file to analyse complexity and security issues", type=[".py"])
+    
+        if uploaded_file is not None:
+            st.divider()
+            file_content = uploaded_file.read().decode("utf-8")
+            st.text_area("Code", value=file_content, height=300, disabled=True, help="This area displays the content of the uploaded file.")
+            # st.button("Run Analysis", type="primary", on_click=calculate_complexity, args=(file_content,))
+            st.write("Complexity Score: " + str(calculate_complexity(file_content)), ) 
+            st.divider()
+
+    with tab2:
+        code_input = st.text_area("Paste your Python code here to analyse complexity and security issues", height=300, key="code_input", help="This area allows you to paste your Python code for analysis.")
+        st.button("Run Analysis", type="primary", on_click=calculate_complexity, args=(code_input,))
+        st.write("Complexity Score: " + str(calculate_complexity(code_input)), ) 
+
+pg = st.navigation([Dashboard, Complexity])
+pg.run()
+
